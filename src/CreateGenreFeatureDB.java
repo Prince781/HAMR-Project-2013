ff658f382d4b44e75cda855e84daa27f11209017
Ben Iofel <iofelben@gmail.com>
2013-06-29 12:09:06 -0400

    started audio code

--- src/CreateGenreFeatureDB.java
+++ src/CreateGenreFeatureDB.java
@@ -4,12 +4,17 @@ import javax.sound.sampled.AudioSystem;
 
 public class CreateGenreFeatureDB {
 	public static final String[] genres = {"blues","classical","country","disco","hiphop","jazz","metal","pop","reggae","rock"};
-	
 	public static void main(String[] args) throws Exception{
 		AudioInputStream audioStrm;
+		int totalFramesRead = 0, bytesPerFrame, numBytes;
+		
 		for(int genreIndex=0;genreIndex<genres.length;genreIndex++) for(int sampleIndex=0;sampleIndex<100;sampleIndex++) {
 			String filepath = "genresamples/" + genres[genreIndex] + "/" + genres[genreIndex] + "." + String.format("%05d", sampleIndex) + ".au";
 			audioStrm = AudioSystem.getAudioInputStream(new File(filepath));
+			bytesPerFrame = audioStrm.getFormat().getFrameSize();
+			//if the audio format has an unspecified frame size, read any amount of bytes
+			if(bytesPerFrame == AudioSystem.NOT_SPECIFIED) bytesPerFrame = 1;
+			byte[] audioBytes = new byte[1024 * bytesPerFrame];
 		}
 	}
 }
