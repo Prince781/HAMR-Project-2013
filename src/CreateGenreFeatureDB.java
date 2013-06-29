e45434e9cd1b5e12fe0b3f7a8466ffb01b3518a8
Ben Iofel <iofelben@gmail.com>
2013-06-29 11:09:19 -0400

    now loops through files

--- src/CreateGenreFeatureDB.java
+++ src/CreateGenreFeatureDB.java
@@ -1,5 +1,15 @@
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+
 public class CreateGenreFeatureDB {
-	public static void main(String[] args) {
-		
+	public static final String[] genres = {"blues","classical","country","disco","hiphop","jazz","metal","pop","reggae","rock"};
+	
+	public static void main(String[] args) throws Exception{
+		AudioInputStream audioStrm;
+		for(int genreIndex=0;genreIndex<genres.length;genreIndex++) for(int sampleIndex=0;sampleIndex<100;sampleIndex++) {
+			String filepath = "genresamples/" + genres[genreIndex] + "/" + genres[genreIndex] + "." + String.format("%05d", sampleIndex) + ".au";
+			audioStrm = AudioSystem.getAudioInputStream(new File(filepath));
+		}
 	}
 }
--- src/GenreRecognizer.java
+++ src/GenreRecognizer.java
@@ -1,5 +1,5 @@
 public class GenreRecognizer {
 	public static void main(String[] args) {
-
+		
 	}
 }
