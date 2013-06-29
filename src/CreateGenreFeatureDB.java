21b92f8994c5c2911870f5f7ba56929bdc31d51a
Ben Iofel <iofelben@gmail.com>
2013-06-29 10:41:59 -0400

    "first files"

---
+++ src/CreateGenreFeatureDB.java
@@ -0,0 +1,5 @@
+public class CreateGenreFeatureDB {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/GenreRecognizer.java
@@ -0,0 +1,5 @@
+public class GenreRecognizer {
+	public static void main(String[] args) {
+
+	}
+}
