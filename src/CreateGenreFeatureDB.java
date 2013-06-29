3eb36b0fd73f004f63a4cc9d361d888a1509608e
Ben Iofel <iofelben@gmail.com>
2013-06-29 15:16:21 -0400

    file io base

--- src/CreateGenreFeatureDB.java
+++ src/CreateGenreFeatureDB.java
@@ -1,7 +1,6 @@
 import java.io.*;
-
+import java.util.*;
 import javax.sound.sampled.*;
-
 import jAudioFeatureExtractor.jAudioTools.*;
 
 public class CreateGenreFeatureDB {
@@ -10,20 +9,28 @@ public class CreateGenreFeatureDB {
 
 	public static void main(String[] args) throws Exception{
 		File curSample;
-		FeatureExtraction extractor;
 		AudioInputStream audioStrm;
 		AudioSamples sample;
+		HashMap<String, Double> curSampleFeatures;
+		List<Map<String, Double>> genreAvgFeatures = new ArrayList<Map<String, Double>>();
+		
 		for(int genreIndex = 0; genreIndex < numGenres; genreIndex++) {
 			for(int sampleIndex = 0; sampleIndex < samplesPerGenre; sampleIndex++) {
 				curSample = new File("genresamples/" + 												//samples folder
 						genres[genreIndex] + "/" + 													//genre folder
 						genres[genreIndex] + "." + String.format("%05d", sampleIndex) + ".au");		//audio file
-				if (!java.nio.file.Files.exists(curSample.toPath())) throw new Exception();			//make sure it exists
+				if (!java.nio.file.Files.exists(curSample.toPath())) throw new FileNotFoundException();	//make sure it exists
 				
 				audioStrm = AudioSystem.getAudioInputStream(curSample);								//load .au file
-				sample = new AudioSamples(audioStrm, (genres[genreIndex]+sampleIndex), true);		//sample object for feature extraction			
-				extractor = new FeatureExtraction(sample);											//switch to the FeatureExtraction class
+				sample = new AudioSamples(audioStrm, (genres[genreIndex]+sampleIndex), true);		//sample object for feature extraction
+				curSampleFeatures = new FeatureExtraction(sample).getFeatureData();
 			}
 		}
+		
+		//average or range the feature data here
+		
+		ObjectOutputStream dbWriteStream = new ObjectOutputStream(new FileOutputStream(new File("genres.db")));
+		dbWriteStream.writeObject(genreAvgFeatures);
+		dbWriteStream.close();
 	}
 }
