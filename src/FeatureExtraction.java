fa6ca11eb852012db0a3b40a46fd196684375b13
Prince781 <princetonf@optonline.net>
2013-06-29 13:04:23 -0400

    Enabled checking for file consistency.

--- src/FeatureExtraction.java
+++ src/FeatureExtraction.java
@@ -1,5 +1,11 @@
+import java.io.*;
+import java.nio.file.*;
 import jAudioFeatureExtractor.*;
 
 public class FeatureExtraction {
-	
+	public FeatureExtraction(File file) throws Exception {
+		if (!Files.exists(file.toPath()))
+			throw new Exception();
+		
+	}
 }
