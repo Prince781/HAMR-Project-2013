dde12906ede57577490761c2e16a6fb6b772833f
Prince781 <princetonf@optonline.net>
2013-06-30 16:46:30 -0400

    Adding code.

---
+++ src/GenreAnalysis.java
@@ -0,0 +1,74 @@
+import jAudioFeatureExtractor.jAudioTools.AudioSamples;
+import jAudioFeatureExtractor.AudioFeatures.*;
+
+import java.io.*;
+import java.util.Random;
+
+public class GenreAnalysis {
+	private static final boolean optimize = false;
+		//whether or not to optimize the process
+	private static final double clipLength = 3.0;
+		/* the maximum clip length of the taken sample from
+		 * our example audio source
+		 */
+	public static double[] mfccData(double[] sample_data, double sample_rate) throws Exception {
+		MFCC mfcc = new MFCC();
+		MagnitudeSpectrum ms = new MagnitudeSpectrum();
+		double[] msData = ms.extractFeature(sample_data, sample_rate, null);
+		return mfcc.extractFeature(sample_data, sample_rate, new double[][]{msData});
+	}
+	public static double[] beatSum(double[] sample_data, double sample_rate) throws Exception {
+		BeatSum bSum = new BeatSum();
+		BeatHistogram bH = new BeatHistogram();
+		RMS rms = new RMS();
+		double[] rmsData = rms.extractFeature(sample_data, sample_rate, null);
+		double[][] rmsValues = new double[256][rmsData.length];
+		for (int i=0; i<256; i++) rmsValues[i] = rmsData;
+			//copy into 256 windows
+		double[] bHData = bH.extractFeature(sample_data, sample_rate, rmsValues);
+		return bSum.extractFeature(sample_data, sample_rate, new double[][]{bHData});
+	}
+	public static double[] spectralFlux(double[] sample_data, double sample_rate) throws Exception {
+		SpectralFlux sf = new SpectralFlux();
+		MagnitudeSpectrum ms = new MagnitudeSpectrum();
+		double[] msData = ms.extractFeature(sample_data, sample_rate, null);
+		double[][] msDataVals = new double[2][msData.length];
+		for (int i=0; i<2; i++)
+			msDataVals[i] = msData;
+		return sf.extractFeature(sample_data, sample_rate, msDataVals);
+	}
+	public static double[] spectralCentroid(double[] sample_data, double sample_rate) throws Exception {
+		SpectralCentroid sc = new SpectralCentroid();
+		PowerSpectrum ps = new PowerSpectrum();
+		double[] psData = ps.extractFeature(sample_data, sample_rate, null);
+		return sc.extractFeature(sample_data, sample_rate, new double[][]{psData});
+	}
+	public static double mean(double[] data_set) {
+		double sum = 0;
+		for (int i=0; i<data_set.length; i++) sum += data_set[i];
+		return sum/data_set.length;
+	}
+	public static void main(String[] args) throws Exception {
+		/* get genre information about the sample, and then compare it to
+		 * our database
+		 */
+		AudioSamples as = new AudioSamples(new File("sound/fireflies.wav"), "Demo Sound", false);
+		double endClip = clipLength + (new Random()).nextDouble()*(as.getDuration()-clipLength);
+			//speed tweak: only compare a few seconds of the song, at random
+		as.normalize(); //normalize the audio: combine channels into one
+		double[] samples = optimize ? as.getSamplesChannelSegregated(endClip-clipLength, endClip)[0] : as.getSamplesMixedDown();
+			//here, we have a random sampled portion of music to work with
+		double[] averages = {
+			mean(mfccData(samples, as.getSamplingRateAsDouble())),
+			mean(beatSum(samples, as.getSamplingRateAsDouble())),
+			mean(spectralCentroid(samples, as.getSamplingRateAsDouble())) 
+		};
+		GenreDB.createDB();
+		
+		//debug
+		System.out.println("Our song: ");
+		for (double n:averages) System.out.print(n+", ");
+		System.out.println();
+		//TODO: System.out.println(<<<guessed genre>>>);
+	}
+}
---
+++ src/GenreDB.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.util.HashMap;
+
+import jAudioFeatureExtractor.AudioFeatures.*;
+import jAudioFeatureExtractor.jAudioTools.*;
+
+public class GenreDB {
+	private static final double sampleCount = 3.0;
+	private static String[] genres = {
+		"blues", "classical", "country", "disco", "hiphop",
+		"jazz", "metal", "pop", "reggae", "rock"
+	};
+	private static HashMap<String, double[]> genreVals  = new HashMap<String, double[]>();
+		//a hashmap containing information regarding the average values for each
+		//particular genre
+	public static void printGenreValues() {
+		for (String genre:genreVals.keySet()) {
+			System.out.print(genre+": "); 
+			for (double n:genreVals.get(genre))
+				System.out.print(n+", ");
+			System.out.println();
+		}
+	}
+	public static void createDB() throws Exception {
+		//analyze genre examples and generate a database of findings; compare later
+		for (String genre:genres) {
+			double[] averageVals = new double[3];
+				/* [0] - contains average MFCC
+				 * [1] - contains spectral flux
+				 * [2] - contains spectral centroid
+				 */
+			double MFCC_s = 0; //mfcc sum
+			//double sF_s = 0; //spectral flux sum
+			double bS_s = 0; //beat sum sum
+			double sC_s = 0; //spectral centroid sum
+			for (int i=1; i<=sampleCount; i++) {
+				//100 is the maximum number of samples per genre
+				AudioSamples as = new AudioSamples(new File("sound/genresamples/"+genre+"/"+genre+"."+String.format("%05d", i)+".au"), 
+						genre+" "+i, false);
+				as.normalize();
+				double[] samples = as.getSamplesMixedDown();
+				MFCC_s += GenreAnalysis.mean(GenreAnalysis.mfccData(samples, as.getSamplingRateAsDouble()));
+				bS_s += GenreAnalysis.mean(GenreAnalysis.beatSum(samples, as.getSamplingRateAsDouble()));
+				sC_s += GenreAnalysis.mean(GenreAnalysis.spectralCentroid(samples, as.getSamplingRateAsDouble()));
+				System.out.println("Working with "+genre+" "+i);
+			}
+			averageVals[0] = MFCC_s/sampleCount;
+			averageVals[1] = bS_s/sampleCount;
+			averageVals[2] = sC_s/sampleCount;
+			genreVals.put(genre, averageVals); //put average values in for each genre
+		}
+		printGenreValues(); //debug
+	}
+}
